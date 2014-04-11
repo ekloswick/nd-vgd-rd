6180ad2a2f610b66121d6579988fda5b1d92a272
vkillion@nd.edu <vkillion@nd.edu@15e62d05-ef32-6ca1-9562-6f1514cf4fd1>
2014-04-11 06:29:41 +0000

    Worked on Character and Camera Control

--- trunk/Assets/Scripts/CameraController.cs
+++ trunk/Assets/Scripts/CameraController.cs
@@ -7,14 +7,20 @@ public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		transform.eulerAngles = new Vector3 (45f, 0f, 0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		Vector3 playerpos = player.transform.position;
+		float playerang = player.transform.eulerAngles.y;
 
-		transform.position = new Vector3 (playerpos.x, 15f, playerpos.z - 10f);
+		transform.eulerAngles = new Vector3 (45f, playerang, 0f);
+
+		float cameraoffx = 10 * Mathf.Sin (playerang * Mathf.Deg2Rad);
+		float cameraoffz = 10 * Mathf.Cos (playerang * Mathf.Deg2Rad);
+
+		transform.position = new Vector3 (playerpos.x - cameraoffx, 15f, playerpos.z - cameraoffz);
 	}
 }
