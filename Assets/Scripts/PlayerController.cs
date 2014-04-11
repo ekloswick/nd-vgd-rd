61a8df50d89e2f2c2025b458e3e1daecdc5f13b3
vkillion@nd.edu <vkillion@nd.edu@15e62d05-ef32-6ca1-9562-6f1514cf4fd1>
2014-04-11 06:29:41 +0000

    Worked on Character and Camera Control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,28 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float rotatespeed;
 
+	public string L_XAxisname;
+	public string L_YAxisname;
+	public string R_XAxisname;
+	public string R_YAxisname;
+	public string Triggers_name;
+
 	// Use this for initialization
 	void Start () {
+
+		if(Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor){
+			L_XAxisname = "L_XAxis_Win";
+			L_YAxisname = "L_YAxis_Win";
+			R_XAxisname = "R_XAxis_Win";
+			R_YAxisname = "R_YAxis_Win";
+		}
+
+		if(Application.platform == RuntimePlatform.OSXPlayer || Application.platform == RuntimePlatform.OSXEditor){
+			L_XAxisname = "L_XAxis_OSX";
+			L_YAxisname = "L_YAxis_OSX";
+			R_XAxisname = "R_XAxis_OSX";
+			R_YAxisname = "R_YAxis_OSX";
+		}
 	
 	}
 	
@@ -15,17 +35,26 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 	
 		//Moving the player based on the left stick
-		float xmotion = Input.GetAxis ("L_XAxis_1");
-		float zmotion = -1 * Input.GetAxis ("L_YAxis_1");
+		float xinput = Input.GetAxis (L_XAxisname);
+		float zinput = -1 * Input.GetAxis (L_YAxisname);
+		float playerang = transform.eulerAngles.y;
 
-		float newx = transform.position.x + (xmotion * speed * Time.deltaTime);
-		float newz = transform.position.z + (zmotion * speed * Time.deltaTime);
+		float xmotion1 = xinput * speed * Mathf.Sin ((playerang + 90) * Mathf.Deg2Rad) * Time.deltaTime;
+		float zmotion1 = xinput * speed * Mathf.Cos ((playerang + 90) * Mathf.Deg2Rad) * Time.deltaTime;
 
-		Vector3 newpos = new Vector3 (newx, 0f, newz);
-		transform.position = newpos;
+		float xmotion2 = zinput * speed * Mathf.Sin (playerang * Mathf.Deg2Rad) * Time.deltaTime;
+		float zmotion2 = zinput * speed * Mathf.Cos (playerang * Mathf.Deg2Rad) * Time.deltaTime;
+
+		float newx = transform.position.x + xmotion1 + xmotion2;
+		float newz = transform.position.z + zmotion1 + zmotion2;
+
+		//float newx = transform.position.x + (xmotion * speed * Time.deltaTime);
+		//float newz = transform.position.z + (zmotion * speed * Time.deltaTime);
+
+		transform.position = new Vector3 (newx, 0f, newz);
 
 		//Roatating the player based on right stick
-		float lookx = Input.GetAxis ("R_XAxis_1");
+		float lookx = Input.GetAxis (R_XAxisname);
 		//float lookz = -1 * Input.GetAxis ("R_YAxis_1");
 
 		float newrotate = Mathf.Sign (lookx) * rotatespeed * Time.deltaTime;
