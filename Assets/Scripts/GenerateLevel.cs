9c640b5abc4150cbafcec44ef8da2e49699b5453
ekloswic@nd.edu <ekloswic@nd.edu@15e62d05-ef32-6ca1-9562-6f1514cf4fd1>
2014-04-13 06:02:12 +0000

    Refined dungeon generator; prior commit added player animations and collisions

--- Assets/Scripts/GenerateLevel.cs
+++ Assets/Scripts/GenerateLevel.cs
@@ -4,21 +4,27 @@ using System.Collections.Generic;
 
 public class GenerateLevel : MonoBehaviour
 {
-	public int levelSize = 50; // must be even
-	public int roomSize = 1;
+	public int levelSize = 50;
+	public int roomSize = 5;
+	public float tileSpawnChance = 0.75f;
+	public float roomSpawnChance = 0.008f;
 	
 	private int[,] levelMatrix;
 	private List<int[]> rooms = new List<int[]>();
-	private GameObject blankTile, darknessTile;
-	
-	private float tileSpawnChance = 0.7f;
-	private float roomSpawnChance = 0.004f;
+	private GameObject blankTile,
+						corridorTile,
+						darknessTile,
+						enemyTile,
+						treasureTile,
+						trapTile;
 	
 	// Use this for initialization
 	void Start ()
 	{
 		// use to get reliable dungeons
 		//Random.seed = 140394581;
+		//Random.seed = 608781738;
+		
 		Debug.Log (Random.seed);
 		
 		levelMatrix = new int[levelSize,levelSize];
@@ -28,6 +34,12 @@ public class GenerateLevel : MonoBehaviour
 		
 		// create the dungeon
 		generateDungeon();
+		
+		// populate dungeon with actual stuff
+		populateDungeon();
+		
+		// scan dungeon to make small tweaks
+		refineDungeon();
 		
 		// draw the dungeon
 		showDungeon();
@@ -48,7 +60,11 @@ public class GenerateLevel : MonoBehaviour
 	void loadPrefabs()
 	{
 		blankTile = (GameObject)Resources.Load ("Prefabs/Floor Tile - Blank");
+		corridorTile = (GameObject)Resources.Load ("Prefabs/Floor Tile - Corridor");
 		darknessTile = (GameObject)Resources.Load ("Prefabs/Floor Tile - Darkness");
+		enemyTile = (GameObject)Resources.Load ("Prefabs/Floor Tile - Enemy");
+		treasureTile = (GameObject)Resources.Load ("Prefabs/Floor Tile - Treasure");
+		trapTile = (GameObject)Resources.Load ("Prefabs/Floor Tile - Trap");
 	}
 	
 	// fills dungeon matrix with empty "0" values (correspond to "darkness" tiles)
@@ -69,7 +85,7 @@ public class GenerateLevel : MonoBehaviour
 	void generateDungeon()
 	{
 		// add default room, always in every level
-		rooms.Add(new int[2] {levelSize/2,levelSize/2});
+		rooms.Add(new int[2] {levelSize/2,levelSize/2}); // could also be something like 10,10
 		
 		float randomNum;
 		
@@ -78,8 +94,8 @@ public class GenerateLevel : MonoBehaviour
 		{
 			for (int z = 0; z < levelMatrix.GetLength(1); z++)
 			{
-				// edge case, always darkness
-				if (x == 0 || z == 0 || x == levelMatrix.GetLength(0) - 1 || z == levelMatrix.GetLength(1) - 1)
+				// edge cases, always darkness
+				if (x <= 1 || z <= 1 || x >= levelMatrix.GetLength(0) - 2 || z >= levelMatrix.GetLength(1) - 2)
 				{
 					levelMatrix[x,z] = 0;
 					continue;
@@ -105,7 +121,7 @@ public class GenerateLevel : MonoBehaviour
 			expandRoom(item[0], item[1], Random.Range (4, 4+roomSize));
 		}
 		
-		// calculate corridors
+		// create corridors
 		generateCorridors();
 		
 		return;
@@ -129,7 +145,7 @@ public class GenerateLevel : MonoBehaviour
 			{					
 				if (levelMatrix[x+i,z+j] == 0 && (x+i != 0) && (x+i != levelMatrix.GetLength(0) - 1) && (z+j != 0) && (z+j != levelMatrix.GetLength(1) - 1))
 				{
-					if (Random.value < tileSpawnChance)// || (i == 0 || j == 0))
+					if (Random.value < tileSpawnChance)
 					{
 						//Debug.Log ("Expanding tile " + (x+i) + ", " + (z+j) + ", Lifetime = " + lifetime);
 						expandRoom((x+i), (z+j), lifetime);
@@ -141,12 +157,13 @@ public class GenerateLevel : MonoBehaviour
 		return;
 	}
 	
+	// creates a relative neighbourhood graph of room nodes and connects them with corridors
 	void generateCorridors()
 	{
 		int roomOne = 0, roomTwo = 0;
 		float maxDistance = 0.0f;
 	
-		// horrible O(n^3) approach to finding relative neighbourhood graph
+		// horrible O(n^3) approach for finding relative neighbourhood graph
 		for (int i = 0; i < rooms.Count; i++)
 		{
 			for (int j = i + 1; j < rooms.Count; j++)
@@ -179,38 +196,61 @@ public class GenerateLevel : MonoBehaviour
 				}
 				
 				if (isEdge)
-				{
 					spawnCorridor(rooms[i], rooms[j]);
-				}
 				
 			}
 		}
 		
-		// create overarching corridor to hopefully make some loops
+		// create overarching corridor to hopefully make some cheap loops
 		spawnCorridor(rooms[roomOne], rooms[roomTwo]);
 		
 		return;
 	}
 	
+	// returns distance between points a and b
 	float distance(int[] a, int[] b)
 	{
 		return Mathf.Sqrt( Mathf.Pow(a[0]-b[0],2) + Mathf.Pow(a[1]-b[1],2) );
 	}
 	
-	// creates L-corridor
-	// TODO make corridors wider
+	// creates L-corridor of varying size from point a to point b
 	void spawnCorridor(int[] a, int[] b)
 	{
+		int corridorWidth = 1;
+		int corridorColor = 5; // 5 for white, 11 for green
+		
+		// decide how wide these corridors will be
+		float tempCorridor = Random.value;
+		
+		if (tempCorridor > 0.85f)
+			corridorWidth = 3;
+		else
+			corridorWidth = 2;
+		
 		int xSlope = b[0] - a[0];
 		int zSlope = b[1] - a[1];
 		
-		//Debug.Log ("X-Slope: " + xSlope + ", Z-Slope: " + zSlope);
-	
+		// first handle side corresponding to the X-axis
 		if (xSlope > 0)
 		{
 			while (xSlope > 0)
 			{
-				levelMatrix[a[0],a[1]] = 5;
+				levelMatrix[a[0],a[1]] = corridorColor;
+				
+				switch (corridorWidth)
+				{
+					case 2:
+						if (a[1] != 1 || a[1] != levelSize - 2)
+							levelMatrix[a[0],a[1]-1] = corridorColor;
+						break;
+					case 3:
+						if (a[1] != 1 || a[1] != levelSize - 2)
+						{
+							levelMatrix[a[0],a[1]-1] = corridorColor;
+							levelMatrix[a[0],a[1]+1] = corridorColor;
+						}
+						break;
+				}
 				
 				xSlope--;
 				a[0]++;
@@ -220,18 +260,49 @@ public class GenerateLevel : MonoBehaviour
 		{
 			while (xSlope < 0)
 			{
-				levelMatrix[a[0],a[1]] = 5;
+				levelMatrix[a[0],a[1]] = corridorColor;
+				
+				switch (corridorWidth)
+				{
+					case 2:
+						if (a[1] != 1 || a[1] != levelSize - 2)
+							levelMatrix[a[0],a[1]-1] = corridorColor;
+						break;
+					case 3:
+						if (a[1] != 1 || a[1] != levelSize - 2)
+						{
+							levelMatrix[a[0],a[1]-1] = corridorColor;
+							levelMatrix[a[0],a[1]+1] = corridorColor;
+						}
+						break;
+				}
 				
 				xSlope++;
 				a[0]--;
 			}
 		}
 		
+		// then handle side corresponding to the Z-axis
 		if (zSlope > 0)
 		{
 			while (zSlope > 0)
 			{
-				levelMatrix[a[0],a[1]] = 5;
+				levelMatrix[a[0],a[1]] = corridorColor;
+				
+				switch (corridorWidth)
+				{
+					case 2:
+						if (a[0] != 1 || a[0] != levelSize - 2)
+							levelMatrix[a[0]-1,a[1]] = corridorColor;
+						break;
+					case 3:
+						if (a[0] != 1 || a[0] != levelSize - 2)
+						{
+							levelMatrix[a[0]-1,a[1]] = corridorColor;
+							levelMatrix[a[0]+1,a[1]] = corridorColor;
+						}
+						break;
+				}
 				
 				zSlope--;
 				a[1]++;
@@ -241,10 +312,98 @@ public class GenerateLevel : MonoBehaviour
 		{
 			while (zSlope < 0)
 			{
-				levelMatrix[a[0],a[1]] = 5;
+				levelMatrix[a[0],a[1]] = corridorColor;
+				
+				switch (corridorWidth)
+				{
+					case 2:
+						if (a[0] != 1 || a[0] != levelSize - 2)
+							levelMatrix[a[0]-1,a[1]] = corridorColor;
+						break;
+					case 3:
+						if (a[0] != 1 || a[0] != levelSize - 2)
+						{
+							levelMatrix[a[0]-1,a[1]] = corridorColor;
+							levelMatrix[a[0]+1,a[1]] = corridorColor;
+						}
+						break;
+				}
 				
 				zSlope++;
 				a[1]--;
+			}
+		}
+		
+		return;
+	}
+	
+	// passes over the levelMatrix to put in enemies, treasures, and traps
+	void populateDungeon()
+	{
+		// only go over tiles that can have floor tiles
+		for (int x = 2; x < levelMatrix.GetLength(0) - 2; x++)
+		{
+			for (int z = 2; z < levelMatrix.GetLength(1) - 2; z++)
+			{
+				if (levelMatrix[x,z] == 5)
+				{
+					/*int wallCount = 0;
+					
+					for (int i = -1; i < 2; i++)
+					{
+						for (int j = -1; j < 2; j++)
+						{					
+							if (i == 0 && j == 0)
+								continue;
+							else if (levelMatrix[x+i,z+j] == 0)
+								wallCount++;
+						}
+					}*/
+					
+					// chance to spawn special tiles
+					float specialChance = Random.value;
+					
+					// 13 = enemy, 14 = treasure, 15 = trap
+					if (specialChance > 0.98f)
+						levelMatrix[x,z] = 13;
+					else if (specialChance > 0.975f)
+						levelMatrix[x,z] = 14;
+					else if (specialChance > 0.95f)
+						levelMatrix[x,z] = 15;
+				}
+			}
+		}
+		
+		return;
+	}
+	
+	// passes over the levelMatrix to check each tile for possible tweaks
+	void refineDungeon()
+	{
+		// only go over tiles that can have floor tiles
+		for (int x = 1; x < levelMatrix.GetLength(0) - 1; x++)
+		{
+			for (int z = 1; z < levelMatrix.GetLength(1) - 1; z++)
+			{
+				if (levelMatrix[x,z] == 5)
+				{
+					int wallCount = 0;
+					
+					for (int i = -1; i < 2; i++)
+					{
+						for (int j = -1; j < 2; j++)
+						{					
+							if (i == 0 && j == 0)
+								continue;
+							else if (levelMatrix[x+i,z+j] == 0)
+								wallCount++;
+						}
+					}
+					
+					// fill in unreachable floor tile
+					if (wallCount >= 5)
+						levelMatrix[x,z] = 0;
+				}
 			}
 		}
 		
@@ -258,7 +417,7 @@ public class GenerateLevel : MonoBehaviour
 		{
 			for (int z = 0; z < levelMatrix.GetLength(1); z++)
 			{
-				// code for if we want to not draw "darkness" tiles; if we want to do custom directioned-walled tiles instead of having "darkness" be the walls
+				// code for if we want to not draw "darkness" tiles, i.e. if we want to do custom directioned-walled tiles instead of having "darkness" be the walls
 				/*if (levelMatrix[x,z] == 0)
 					continue;
 					*/
@@ -277,32 +436,20 @@ public class GenerateLevel : MonoBehaviour
 	{
 		switch (tileNum)
 		{
-			case 1:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			case 2:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			case 3:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			case 4:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			case 5:
 				return (GameObject)Instantiate(blankTile);
-			case 6:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			case 7:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			case 8:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			case 9:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			case 10:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			case 11:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			case 22:
-				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			default:
+				return (GameObject)Instantiate(corridorTile);
+			case 13:
+				return (GameObject)Instantiate(enemyTile);
+			case 14:
+				return (GameObject)Instantiate(treasureTile);
+			case 15:
+				return (GameObject)Instantiate(trapTile);
+			case 0:
 				return (GameObject)Instantiate(darknessTile);
+			default:
+				return GameObject.CreatePrimitive(PrimitiveType.Sphere);
 		}
 	}
 }
