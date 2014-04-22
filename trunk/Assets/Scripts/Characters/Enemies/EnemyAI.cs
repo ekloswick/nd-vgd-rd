820dca1d4287756b806204832079fb3b7e28ec3d
ekloswic@nd.edu <ekloswic@nd.edu@15e62d05-ef32-6ca1-9562-6f1514cf4fd1>
2014-04-22 21:59:54 +0000

    Added preliminary textures, as well as removed some duplicate scripts from the repository

---
+++ trunk/Assets/Scripts/Characters/Enemies/EnemyAI.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyAI : MonoBehaviour {
+
+	public float chaseSpeed = 1.8f;
+	public float wanderSpeed = 1.0f;
+	public float chaseTurnRate = 200f;
+	public float wanderTurnRate = 100f;
+	public float desiredDistance;
+	
+	public float FOVAngle = 90f; // 45 would mean 90 total FOV (45 on each side)
+	public float FOVDistance = 15f; // how many units away in the FOV can they see
+	
+	public bool targetIsFromMemory = false;
+	
+	public GameObject playerReference;
+	public Vector3 currentTarget;
+	public Vector3 targetDirection;
+	private Vector3[] objectChecks = {new Vector3(0f,0f,1f), new Vector3(0f,0f,-1f), new Vector3(1f,0f,0f), new Vector3(-1f,0f,0f),
+									new Vector3(1f,0f,1f), new Vector3(-1f,0f,-1f), new Vector3(1f,0f,-1f), new Vector3(-1f,0f,1f)};
+	
+	// moves enemy towards currentTarget
+	public void moveToTarget(float speed, float turnRate)
+	{
+		transform.position = transform.position + speed*transform.forward * Time.deltaTime;
+		
+		targetDirection = currentTarget - transform.position;
+		
+		// calculate cross product between forward vector and vector connecting enemy and player
+		if (Vector3.Cross(transform.forward, targetDirection).y < 0f)
+			transform.Rotate(transform.up, -turnRate*Time.deltaTime);
+		else
+			transform.Rotate(transform.up, turnRate*Time.deltaTime);
+		
+		return;
+	}
+	
+	// tries to keep some distance away from objects
+	public void obstacleAvoidance()
+	{
+		// cast 8 raycasts out from enemy, looking for obstacles
+		// if obstacles are found, make sure distance from them is greater than 0.2
+		foreach (Vector3 dir in objectChecks)
+		{
+			RaycastHit hit = new RaycastHit();
+			//Debug.DrawRay (transform.position, dir, Color.red, 0.5f);
+			
+			// performs raycast calcuations, 9 = wall layer
+			if (Physics.Raycast(transform.position, dir, out hit, 1f, (1 << 9 | 1 << 11 | 1 << 12)))
+			{
+				//Debug.Log("Wall detected: " + dir);
+				//Debug.DrawRay (transform.position, dir, Color.red, 0.5f);
+				
+				// if raycast hits wall that's closer than 0.5 units away
+				if ((hit.transform.tag == "Wall" || hit.transform.tag == "Enemy" || hit.transform.tag == "Chest") && hit.distance < 0.5f)
+				{
+					//Debug.Log("Pushing away from wall: " + dir);
+					transform.position = transform.position - 0.7f*dir*Time.deltaTime;
+				}
+			}
+		}
+		
+		return;
+	}
+	
+	// return true or false based on if enemy has unobstructed LOS to player
+	public bool canSeePlayer()
+	{
+		RaycastHit hit = new RaycastHit();
+		Vector3 playerDirection = playerReference.transform.position - transform.position;
+		
+		// if player is in field of view and raycast isn't blocked
+		if (Vector3.Angle(playerDirection, transform.forward) < FOVAngle || Vector3.Distance(playerReference.transform.position, transform.position) < 5.0f)
+		{
+			// performs raycast calcuations, ~(1 << 8) is a layer mask saying we want to test every layer BUT 8, which is floor
+			// if floor is not cut out of tests, enemies won't move since transform.position is at y = 0
+			if (Physics.Raycast(transform.position, playerDirection, out hit, FOVDistance, ~(1 << 8 | 1 << 14)))
+			{
+				//Debug.DrawRay (transform.position, playerDirection, Color.red, 2);
+				
+				if (hit.transform.tag == "Player" || hit.transform.tag == "PlayerModel")
+				{
+					currentTarget = hit.transform.position;
+					//targetDirection = playerDirection;
+					
+					targetIsFromMemory = false;
+					
+					return true;
+				}
+				else
+					return false;
+			}
+		}
+		
+		return false;
+	}
+	
+	// checks if enemy is within range and can actually attack player
+	public bool canAttackPlayer()
+	{
+		if (Vector3.Distance(playerReference.transform.position, transform.position) < desiredDistance)
+			return true;
+		else
+			return false;
+	
+		//Debug.Log (distanceToTarget() + " and " + desiredDistance);
+		/*if (targetIsFromMemory)
+		{
+			if (distanceToTarget() < 0.1f)
+				return true;
+		}
+		else if (distanceToTarget() < desiredDistance)
+			return true;
+		else
+			return false;
+			
+		return false;*/
+	}
+	
+	// checks for direct LOS to smellpoint
+	public bool canSeeSmellPoint()
+	{
+		RaycastHit hit = new RaycastHit();;
+		Vector3 smellPointDirection;
+		
+		foreach (SmellPoint item in playerReference.GetComponent<PlayerStats>().smellPoints)
+		{
+			smellPointDirection = item.point - transform.position;
+			
+			// if player is in field of view and raycast isn't blocked
+			if (Vector3.Angle(smellPointDirection, transform.forward) < FOVAngle)
+			{
+				// 8 = floor, 11 = enemies, 12 = chests, 13 = items, 15 = traps
+				if (Physics.Raycast(transform.position, smellPointDirection, out hit, FOVDistance, ~(1 << 8 | 1 << 11 | 1 << 12 | 1 << 13 | 1 << 15)))
+				{
+					//Debug.DrawRay (transform.position, smellPointDirection, Color.blue, 0.4f);
+					if (hit.transform.tag == "SmellPoint")
+					{
+						// set target variables
+						currentTarget = hit.transform.position;
+						//targetDirection = smellPointDirection;
+						
+						// target might get deleted, so notify logic that it might go to a "memory" target
+						targetIsFromMemory = true;
+						
+						return true;
+					}
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	// checks for LOS to wanderpoint
+	public bool canSeeWanderPoint()
+	{
+		
+		return false;
+	}
+	
+	// executes attack
+	public void attackPlayer()
+	{
+		
+		return;
+	}
+	
+	// helper function to calculate absolute distance from enemy to target
+	public float distanceToTarget()
+	{
+		return Vector3.Distance(transform.position, currentTarget);
+	}
+}
