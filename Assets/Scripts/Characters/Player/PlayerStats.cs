a63b4dffbc5b76acf995d708199fe35d6fb30626
ekloswic@nd.edu <ekloswic@nd.edu@15e62d05-ef32-6ca1-9562-6f1514cf4fd1>
2014-04-24 21:14:40 +0000

    Placed sword and shield in player hands, can attack and kill enemies

--- Assets/Scripts/Characters/Player/PlayerStats.cs
+++ Assets/Scripts/Characters/Player/PlayerStats.cs
@@ -4,23 +4,39 @@ using System.Collections.Generic;
 
 public class PlayerStats : CharacterStats {
 
-	public GameObject righthand;
-	public GameObject lefthand;
+	//public GameObject currentWeapon;
+	//public GameObject currentShield;
 
-	private Animator myAnim;
+	public Animator myAnim;
+	public int currentLevel;
+	public GameObject currentWeapon, currentShield;
 	
+	/*[HideInInspector]
+	public GameObject currentSpell;*/
 	[HideInInspector]
 	public List<SmellPoint> smellPoints = new List<SmellPoint>();
 	
 	// Use this for initialization
 	void Start ()
 	{
-		//myAnim = GetComponent<Animator>();
+		myAnim = GetComponent<Animator>();
 		totalHealth = 3;
 		currentHealth = 3;
-		currentWeapon = 1;
-		currentShield = 1;
+		
+		currentWeapon = (GameObject)Instantiate(Resources.Load("Prefabs/Items/Sword"));
+		currentWeapon.transform.parent = GameObject.Find("Right_Forearm").transform;
+		currentWeapon.transform.position = currentWeapon.transform.parent.position + new Vector3(0.5f,0.2f,0f);
+		
+		currentShield = (GameObject)Instantiate(Resources.Load("Prefabs/Items/Shield"));
+		currentShield.transform.parent = GameObject.Find("Left_Forearm").transform;
+		currentShield.transform.position = currentShield.transform.parent.position + new Vector3(-.05f,.075f,.075f);
+		//lefthand.transform.position = lefthand.transform.parent.position;
+		//lefthand.transform.rotation = lefthand.transform.parent.rotation;
+		currentShield.transform.rotation = currentShield.transform.parent.rotation * Quaternion.Euler(40f, 160f, 80f);
+		
 		currentSpell = 0;
+		
+		currentLevel = 1;
 		
 		// initialize "smellPoints" to allow smarter enemy chasing
 		InvokeRepeating("UpdateSmellPoints", 0, 0.5f);
---
+++ Assets/Scripts/Items/WeaponStats.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponStats : MonoBehaviour {
+
+	public int damage;
+	public float cooldown;
+	//public List<Attribute> attributes = new List<Attribute>();
+	public GameObject playerReference;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerReference = GameObject.FindWithTag("Player");
+		// when weapon is created, give it random properties
+		damage = Random.Range(1+playerReference.GetComponent<PlayerStats>().currentLevel, playerReference.GetComponent<PlayerStats>().currentLevel*2);
+		cooldown = Random.Range (1,5) / 4f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (playerReference.GetComponent<PlayerStats>().myAnim.GetBool("attacking"))
+		{
+			if (other.tag == "Enemy")
+			{
+				// assume melee for now
+				//if (weaponType == "Melee")
+				//else if (weaponType == "Ranged")
+				
+				other.gameObject.GetComponent<EnemyStats>().isAttackedBy(playerReference.GetComponent<PlayerStats>().currentWeapon);
+				
+				Debug.Log ("HIT, Damage: " + damage + ", Enemy HP left: " + other.gameObject.GetComponent<EnemyStats>().currentHealth);
+			}
+		}
+	}
+}
