8a66b29d86135a18ad9a600962b6140658d2fbaa
vkillion@nd.edu <vkillion@nd.edu@15e62d05-ef32-6ca1-9562-6f1514cf4fd1>
2014-04-25 00:21:45 +0000

    Finished updating player controls.

--- Assets/Scripts/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	
 	public float speed;
-	public float rotatespeed;
+	public float notblockrotate;
+	public float blockrotate;
 
 	private string L_XAxisname;
 	private string L_YAxisname;
@@ -13,10 +14,8 @@ public class PlayerController : MonoBehaviour {
 	private string Triggers_name;
 	private string DPad_YAxis_name;
 
-	private float modspeed;
-	private float lookangle;
-
 	private Animator myAnim;
+	private float rotatespeed;
 
 	// Use this for initialization
 	void Start () {
@@ -50,66 +49,78 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
+		turnPlayer ();
+
+		movePlayer ();
+
+	}
+
+	void turnPlayer(){
+
 		if(myAnim.GetCurrentAnimatorStateInfo(0).IsName("Blocking")){
-			modspeed = speed * 0.5f;
+			rotatespeed = blockrotate;
 		} else {
-			modspeed = speed;
+			rotatespeed = notblockrotate;
 		}
-	
+			
+		//Turn the player based on the right stick
+		float xlook = Input.GetAxis (MyInput.R_XAxisname);
+		float zlook = -1 * Input.GetAxis (MyInput.R_YAxisname);
+		
+		float newangle = -1 * (Mathf.Atan2 (zlook, xlook) * Mathf.Rad2Deg - 90);
+		float currangle =  transform.eulerAngles.y;
+
+		//First make sure both angles are positive by adding 360
+		newangle += 360;
+		currangle += 360;
+
+		//Next take the modulus with 360 to get a result between 0 and 360
+		newangle %= 360;
+		currangle %= 360;
+
+		//Lastly add 360 again to get a result between 360 and 720 so we can subtract
+		newangle += 360;
+		currangle += 360;
+
+
+		float difference = newangle - currangle;
+		float direction = Mathf.Sign (difference);
+
+		if(Mathf.Abs (difference) > 180){
+			direction *= -1;
+		}
+
+		//Only rotate if the stick is moved and there is a angle difference > 5
+		if(((Mathf.Abs (xlook) > 0.5f) || (Mathf.Abs (zlook) > 0.5f)) && (Mathf.Abs (difference) > 15f)){
+			transform.Rotate(0f, rotatespeed * direction * Time.deltaTime, 0f);
+		}
+
+		print ((currangle%360) + ", " + (newangle%360));
+	}
+
+	void movePlayer(){
 		//Moving the player based on the left stick
 		float xinput = Input.GetAxis (MyInput.L_XAxisname);
 		float zinput = -1 * Input.GetAxis (MyInput.L_YAxisname);
-
-		//Turn the player based on the right stick
-		float xlook = Input.GetAxis (MyInput.L_XAxisname);
-		float zlook = Input.GetAxis (MyInput.L_YAxisname);
-
-		//Setting the values in the Animation Blend Tree
-		myAnim.SetFloat("forward", xinput);
-		myAnim.SetFloat("strafe", zinput);
 		
 		// alternate, non-Xbox controller controls
 		if (Input.GetKey (KeyCode.W))
 			zinput = 1.0f;
 		else if (Input.GetKey (KeyCode.S))
 			zinput = -1.0f;
-
+		
 		// alternate, non-Xbox controller controls
 		if (Input.GetKey (KeyCode.A))
 			xinput = -1.0f;
 		else if (Input.GetKey(KeyCode.D))
 			xinput = 1.0f;
-
-		if((Mathf.Abs (xinput) > 0.01f || Mathf.Abs (zinput) > 0.01f) && !myAnim.GetCurrentAnimatorStateInfo(0).IsName("Blocking") && !myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attacking")){
-			lookangle = (Mathf.Atan2 (xinput, zinput) * Mathf.Rad2Deg) + CameraController.angle;
-
-			if(Input.GetKey (KeyCode.S))
-				lookangle = (Mathf.Atan2 (xinput, zinput) * Mathf.Rad2Deg) + CameraController.angle + 180;
-
-			if(lookangle > 360)
-				lookangle -= 360;
-			if(lookangle < 0)
-				lookangle += 360;
-
-			transform.eulerAngles = new Vector3(0f, lookangle, 0f);
-		}
-
-		float xmotion1 = xinput * Mathf.Sin ((CameraController.angle + 90) * Mathf.Deg2Rad);
-		float zmotion1 = xinput * Mathf.Cos ((CameraController.angle + 90) * Mathf.Deg2Rad);
-
-		float xmotion2 = zinput * Mathf.Sin (CameraController.angle * Mathf.Deg2Rad);
-		float zmotion2 = zinput * Mathf.Cos (CameraController.angle * Mathf.Deg2Rad);
-
-		float xmotion = xmotion1 + xmotion2;
-		float zmotion = zmotion1 + zmotion2;
-
-		Vector3 newmotion = new Vector3 (xmotion, 0f, zmotion);
-	
-		rigidbody.MovePosition (transform.position + (modspeed * newmotion * Time.deltaTime));
 		
-
-	
-
-
+		//Setting the values in the Animation Blend Tree
+		myAnim.SetFloat("forward", xinput);
+		myAnim.SetFloat("strafe", zinput);
+		
+		Vector3 newmotion = new Vector3 (xinput, 0f, zinput);
+		
+		rigidbody.MovePosition (transform.position + (speed * newmotion * Time.deltaTime));
 	}
 }
