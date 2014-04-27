673fef4ec3894b83c2751e6e5596bef15f8c06f2
vkillion@nd.edu <vkillion@nd.edu@15e62d05-ef32-6ca1-9562-6f1514cf4fd1>
2014-04-27 06:42:22 +0000

    Added shield spawning and pickup. Simplified popup messages.

---
+++ Assets/Scripts/Items/ShieldStats.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldStats : MonoBehaviour {
+	public GUIText popupref;
+	public int turnspeed;
+	public float movespeed;
+	//public List<Attribute> attributes = new List<Attribute>();
+	public GameObject playerReference;
+	
+	private GUIText popup;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		playerReference = GameObject.FindWithTag("Player");
+		
+		// when shield is created, give it random properties
+		GameObject currentshield = playerReference.GetComponent<PlayerStats> ().currentShield;
+		int currturnspeed = currentshield.GetComponent<ShieldStats>().turnspeed;
+		if(currturnspeed == 0){
+			movespeed = 0.33f;
+			turnspeed = 45;
+			renderer.material.color = new Color(0.75f, 0.75f, 0.75f);
+			
+		} else {
+			movespeed = Random.Range(0.33f, 1.0f);
+			turnspeed = Random.Range (45, 180);
+			renderer.material.color = new Color(Random.value, Random.value, Random.value);
+		}
+		
+		popup = (GUIText) Instantiate (popupref);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 playerpos = playerReference.transform.position;
+		Vector3 shieldpos = gameObject.transform.position;
+		
+		if(shieldpos.y < 0f){
+			shieldpos.y = 1f;
+			transform.position = shieldpos;
+		}
+		
+		if(Vector3.Distance(playerpos, shieldpos) < 1 && transform.root.tag != "Player"){
+			
+			float currmovespeed = playerReference.GetComponent<PlayerStats>().currentShield.GetComponent<ShieldStats>().movespeed;
+			int currturnspeed = playerReference.GetComponent<PlayerStats>().currentShield.GetComponent<ShieldStats>().turnspeed;
+
+			popup.text = "A (Xbox)/Q (Key) for shield (Move/Turn: <" + 
+				currmovespeed.ToString("0.##") + ", " + currturnspeed +
+				"> -> <" + movespeed.ToString("0.##") + ", " + turnspeed + ">)";
+			if(Input.GetKeyDown(KeyCode.Q) || Input.GetButtonDown(MyInput.A_name)){
+				playerReference.GetComponent<PlayerStats>().PickUpShield(gameObject);
+			}
+		} else {
+			popup.text = "";
+		}
+		
+	}
+}
