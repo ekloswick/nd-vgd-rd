0666eab9e92a4c1f39cb7eb6b657f793e118971d
vkillion@nd.edu <vkillion@nd.edu@15e62d05-ef32-6ca1-9562-6f1514cf4fd1>
2014-04-27 02:01:15 +0000

    Chests spawn swords. Pause button pauses player and enemy movement and hides HUD. Known bug: advancing levels does not work (missing a reference). Will be fixed soon.

--- Assets/Scripts/Items/ChestController.cs
+++ Assets/Scripts/Items/ChestController.cs
@@ -25,11 +25,32 @@ public class ChestController : MonoBehaviour {
 
 		if(Vector3.Distance(playerpos, chestpos) < 1 && myAnim.GetCurrentAnimatorStateInfo(0).IsName("Closed")){
 			popup.text = "Press A (Xbox) / Q (Keyboard) to open chest";
-			if(Input.GetKeyDown(KeyCode.Q) || Input.GetButton(MyInput.A_name)){
+			if(Input.GetKeyDown(KeyCode.Q) || Input.GetButtonDown(MyInput.A_name)){
 				myAnim.SetBool("isOpen", true);
+				spawnItem(playerpos - chestpos);
 			}
 		} else {
 			popup.text = "";
 		}
+	}
+
+	void spawnItem(Vector3 direction){
+		GameObject newitem;
+		float itemchance = Random.value;
+
+		itemchance = 0f;
+
+		if(itemchance > 0.95){
+			//spawn spell
+		} else if(itemchance > 0.65){
+			//spawn shield
+		} else {
+			newitem = (GameObject)Instantiate(Resources.Load("Prefabs/Items/Sword"));
+			newitem.transform.position = transform.position;
+			newitem.rigidbody.velocity = new Vector3(direction.x, 7f, direction.z);
+			GameObject.Find ("GeneralScripts").GetComponent<GenerateLevel> ().swordList.Add (newitem);
+		}
+
+
 	}
 }
--- Assets/Scripts/Procedural/LevelTransition.cs
+++ Assets/Scripts/Procedural/LevelTransition.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class LevelTransition : MonoBehaviour {
 
+	public GUIText popupref;
+	private GUIText popup;
+
 	// Use this for initialization
 	void Start ()
 	{
-	
+		popup = (GUIText)Instantiate (popupref);
 	}
 	
 	// Update is called once per frame
@@ -19,11 +22,20 @@ public class LevelTransition : MonoBehaviour {
 		if (other.tag == "Player")
 		{
 			//Debug.Log ("Player collision");
-			
+			popup.text = "Press A (Xbox) / Q (Keyboard) to advance to next level";
+
 			if (Input.GetButton(MyInput.A_name))
 			{
 			    GameObject.FindWithTag("GameController").GetComponent<GenerateLevel>().proceedToNextLevel();
 			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			popup.text = "";
 		}
 	}
 }
